b8da126eacfda717d3ef6d3ff9513b4001aa60f5
b4141 <barikmail1@gmail.com>
2024-01-12 17:43:44 +0100

    styled the productPage

--- flaskShopApp/routes.py
+++ flaskShopApp/routes.py
@@ -36,101 +36,112 @@ def productPage(productId):
 allProducts = [
     {
         "id": "1",
-        "name": "product 1",
-        "description": "this is product number 1",
-        "imgs": ["p1.png"],
+        "name": "ANASTASIA",
+        "description": "Eyelash Mascara",
+        "imgs": ["p1-removebg-preview.png"],
         "fullDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "rating": 3,
-        "price": 10
+        "price": 10.99,
+        "reviewsNumber": 123
     },
     {
         "id": "2",
-        "name": "product 2",
-        "description": "this is product number 2",
-        "imgs": ["p2.png"],
+        "name": "HOURGLASS",
+        "description": "Body Cream",
+        "imgs": ["p2-removebg-preview.png"],
         "fullDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        "rating": 5,
-        "price": 20
+        "rating": 3,
+        "price": 20.99,
+        "reviewsNumber": 123
     },
     {
         "id": "3",
-        "name": "product 3",
-        "description": "this is product number 3",
-        "imgs": ["p3.png"],
+        "name": "stila",
+        "description": "Eye Liner",
+        "imgs": ["p3-removebg-preview.png"],
         "fullDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "rating": 5,
-        "price": 30
+        "price": 30.99,
+        "reviewsNumber": 123
     },
     {
         "id": "4",
         "name": "product 4",
         "description": "this is product number 4",
-        "imgs": ["p4.png"],
+        "imgs": ["p4-removebg-preview.png"],
         "fullDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "rating": 5,
-        "price": 40
+        "price": 40.99,
+        "reviewsNumber": 123
     },
     {
         "id": "5",
         "name": "product 5",
         "description": "this is product number 5",
-        "imgs": ["p5.png"],
+        "imgs": ["p5-removebg-preview.png"],
         "fullDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "rating": 5,
-        "price": 50
+        "price": 50.99,
+        "reviewsNumber": 123
     },
     {
         "id": "6",
         "name": "product 6",
         "description": "this is product number 6",
-        "imgs": ["p6.png"],
+        "imgs": ["p6-removebg-preview.png"],
         "fullDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "rating": 5,
-        "price": 60
+        "price": 60.99,
+        "reviewsNumber": 123
     },
     {
         "id": "7",
         "name": "product 7",
         "description": "this is product number 7",
-        "imgs": ["p7.png"],
+        "imgs": ["p7-removebg-preview.png"],
         "fullDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "rating": 5,
-        "price": 70
+        "price": 70.99,
+        "reviewsNumber": 123
     },
     {
         "id": "8",
         "name": "product 8",
         "description": "this is product number 8",
-        "imgs": ["p8.png"],
+        "imgs": ["p8-removebg-preview.png"],
         "fullDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "rating": 5,
-        "price": 80
+        "price": 80.99,
+        "reviewsNumber": 123
     },
     {
         "id": "9",
         "name": "product 9",
         "description": "this is product number 9",
-        "imgs": ["p9.png"],
+        "imgs": ["p9-removebg-preview.png"],
         "fullDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "rating": 5,
-        "price": 90
+        "price": 90.99,
+        "reviewsNumber": 123
     },
     {
         "id": "10",
         "name": "product 10",
         "description": "this is product number 10",
-        "imgs": ["p10.png"],
+        "imgs": ["p10-removebg-preview.png"],
         "fullDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "rating": 5,
-        "price": 100
+        "price": 100.99,
+        "reviews-Umber": 102
     },
     {
         "id": "11",
         "name": "product 11",
         "description": "this is product number 11",
-        "imgs": ["p11.png"],
+        "imgs": ["p4-removebg-preview.png"],
         "fullDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "rating": 5,
-        "price": 110
+        "price": 110.99,
+        "reviews-Umber": 102
     }
 ]
