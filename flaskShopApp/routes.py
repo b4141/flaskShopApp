f11ea070a86cbf7296d6edafc7b21f93a328a0bf
b4141 <barikmail1@gmail.com>
2024-01-12 23:58:54 +0100

    finshed the product info on the productPage

--- flaskShopApp/routes.py
+++ flaskShopApp/routes.py
@@ -38,110 +38,121 @@ allProducts = [
         "id": "1",
         "name": "ANASTASIA",
         "description": "Eyelash Mascara",
-        "imgs": ["p1-removebg-preview.png"],
+        "imgs": ["p1-1.png"],
         "fullDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "rating": 3,
         "price": 10.99,
-        "reviewsNumber": 123
+        "reviewsNumber": 123,
+        "ingredients": "None"
     },
     {
         "id": "2",
         "name": "HOURGLASS",
         "description": "Body Cream",
-        "imgs": ["p2-removebg-preview.png"],
+        "imgs": ["p2-1.png", "p2-1.png", "p2-1.png"],
         "fullDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "rating": 3,
         "price": 20.99,
-        "reviewsNumber": 123
+        "reviewsNumber": 123,
+        "ingredients": "None"
     },
     {
         "id": "3",
         "name": "stila",
         "description": "Eye Liner",
-        "imgs": ["p3-removebg-preview.png"],
+        "imgs": ["p3-1.png"],
         "fullDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "rating": 5,
         "price": 30.99,
-        "reviewsNumber": 123
+        "reviewsNumber": 123,
+        "ingredients": "None"
     },
     {
         "id": "4",
-        "name": "product 4",
-        "description": "this is product number 4",
-        "imgs": ["p4-removebg-preview.png"],
-        "fullDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        "rating": 5,
+        "name": "Unlocked",
+        "description": "Instant Extensions Mascara",
+        "imgs": ["p4-2.png"],
+        "fullDescription": "The look of lash extensions in an instant. This mascara is the ultimate formula for when more is more: more definition, more lift and more length to transform lashes with impossibly real results. Using film-forming technology, it coats each lash in lightweight fibers that lock in place for a high-impact, fanned-out finish and smudge-proof wear.",
+        "rating": 3,
         "price": 40.99,
-        "reviewsNumber": 123
+        "reviewsNumber": 123,
+        "ingredients": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     },
     {
         "id": "5",
         "name": "product 5",
         "description": "this is product number 5",
-        "imgs": ["p5-removebg-preview.png"],
+        "imgs": ["p5-1.png"],
         "fullDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "rating": 5,
         "price": 50.99,
-        "reviewsNumber": 123
+        "reviewsNumber": 123,
+        "ingredients": "None"
     },
     {
         "id": "6",
         "name": "product 6",
         "description": "this is product number 6",
-        "imgs": ["p6-removebg-preview.png"],
+        "imgs": ["p6-1.png"],
         "fullDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "rating": 5,
         "price": 60.99,
-        "reviewsNumber": 123
+        "reviewsNumber": 123,
+        "ingredients": "None"
     },
     {
         "id": "7",
         "name": "product 7",
         "description": "this is product number 7",
-        "imgs": ["p7-removebg-preview.png"],
+        "imgs": ["p7-1.png"],
         "fullDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "rating": 5,
         "price": 70.99,
-        "reviewsNumber": 123
+        "reviewsNumber": 123,
+        "ingredients": "None"
     },
     {
         "id": "8",
         "name": "product 8",
         "description": "this is product number 8",
-        "imgs": ["p8-removebg-preview.png"],
+        "imgs": ["p8-1.png"],
         "fullDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "rating": 5,
         "price": 80.99,
-        "reviewsNumber": 123
+        "reviewsNumber": 123,
+        "ingredients": "None"
     },
     {
         "id": "9",
         "name": "product 9",
         "description": "this is product number 9",
-        "imgs": ["p9-removebg-preview.png"],
+        "imgs": ["p9-1.png"],
         "fullDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "rating": 5,
         "price": 90.99,
-        "reviewsNumber": 123
+        "reviewsNumber": 123,
+        "ingredients": "None"
     },
     {
         "id": "10",
         "name": "product 10",
         "description": "this is product number 10",
-        "imgs": ["p10-removebg-preview.png"],
+        "imgs": ["p10-1.png"],
         "fullDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "rating": 5,
         "price": 100.99,
-        "reviews-Umber": 102
+        "reviews-Umber": 102,
+        "ingredients": "None"
     },
     {
         "id": "11",
         "name": "product 11",
         "description": "this is product number 11",
-        "imgs": ["p4-removebg-preview.png"],
+        "imgs": ["p4-1.png"],
         "fullDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
         "rating": 5,
         "price": 110.99,
-        "reviews-Umber": 102
+        "reviews-Umber": 102,
+        "ingredients": "None"
     }
 ]
